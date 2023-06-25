533f679e76737ea15fd245874a4dca60b0244f92
murphychuks <murphychuks549@gmail.com>
2023-06-25 06:59:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
-#include stdio.h>
+#include <stdio.h>
 /**
- * main - A program that prints a puts function
+ * main - A program that prints a line with put function
  * Return: 0
  */
 int main(void)
@@ -8,4 +8,3 @@ int main(void)
 puts("Programming is like building a multilingual puzzle");
 return (0);
 }
-
